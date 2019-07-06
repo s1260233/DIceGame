77555b6a7077de4fa0479d3d4648b88d6f21a365
Ito Ryoya <ryoya@ItonoMacBook-puro.local>
2019-07-06 17:28:01 +0900

    Initial revision

---
+++ DiceGame.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int dice1, dice2, total;
+
+  printf("Rolling the dice...\n");
+
+  //initialize random number
+  srand(time(NULL));
+
+  dice1 = rand() % 6 + 1;
+  dice2 = rand() % 6 + 1;
+  total = dice1 + dice2;
+
+  printf("Die 1: %d\n", dice1);
+  printf("Die 2: %d\n", dice2);
+  printf("Total value: %d\n", total);
+  
+  return 0;
+}
