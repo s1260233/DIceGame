8d95bff31ebc3bf14358a1cd7fcff377eac511b5
Ito Ryoya <ryoya@ItonoMacBook-puro.local>
2019-07-06 17:47:12 +0900

    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -24,8 +24,8 @@ int main(){
   printf("Die 2: %d\n", dice2);
   printf("Total value: %d\n", total);
 
-  if(total > 7) printf("You won!\n");
-  else printf("You lost.\n");
+  if(total > 7) printf("%s won!\n", name);
+  else printf("%s lost.\n", name);
   
   return 0;
 }
